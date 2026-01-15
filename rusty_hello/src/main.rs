2fea9bf2579a00e16e75cd4c6cc7bbfec4cf2d49
Mike <asfez.mike@outlook.com>
2026-01-15 12:35:17 +0100

    ajout de l'exo 01 corrigé 667

---
+++ rusty_hello/src/main.rs
@@ -0,0 +1,29 @@
+use clap::Parser;
+
+#[derive(Parser)]
+struct Args {
+    #[arg(default_value = "World")]
+    name: String,
+
+    #[arg(long)]
+    upper: bool,
+
+    #[arg(long, default_value = "1")]
+    repeat: u32,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    let message = if args.upper {
+        format!("HELLO, {}!", args.name.to_uppercase())
+    } else {
+        format!("Hello, {}!", args.name)
+    };
+
+    for _ in 0..args.repeat {
+        println!("{message}");
+    }
+}
+
+// aller c'est bon la
